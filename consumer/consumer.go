cfd97466811d0f9593115895ef5cc62f92f9df51
ritwiksamrat <64915010+ritwiksamrat@users.noreply.github.com>
2020-12-05 17:34:23 +0530

    Add files via upload

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -5,13 +5,24 @@ import (
 	"fmt"
 	// "log"
 	// "os"
-	
+	"time"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
-
+const (  
+    username = "root"
+    password = "root12345"
+    hostname = "127.0.0.1:3306"
+    dbname   = "realinfo"
+)
 func main() {
 	
-
+	db, err := dbConnection()
+	if err!=nil{
+		panic(err.Error())
+		return
+	}
+	defer db.Close()
+	fmt.Println("DataBase is Successfully Connected")
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost",
 		"group.id":          "myGroup",
@@ -31,11 +42,62 @@ func main() {
 		}else {
 			fmt.Printf("Consumer error: %v (%v)\n", err, uname)
 		}
-	}
 
+		currenttime:=time.Now()
+		fmt.Println("Current Time is: ",string(currenttime.Format("15:04")))
+		var op int64
+		fmt.Println("You can give the time difference in hrs or mins or secs")
+		fmt.Println("1. Hours")
+		fmt.Println("2. Minutes")
+		fmt.Println("3. Seconds")
+		fmt.Println("4. Press 4 if you don't want to give the difference")
+		fmt.Scanln(&op)
+		switch op{
+		        case 1:
+						var hors int64
+						fmt.Println("Enter the difference in hours")
+						fmt.Scanln(&hors)
+						hoursfunc(currenttime,hors)
+						break
+				case 2: 
+						var minus int64
+						fmt.Println("Enter the difference in minutes")
+						fmt.Scanln(&minus)
+						minutesfunc(currenttime,minus)
+						break
+				case 3: 
+						var secons int64
+						fmt.Println("Enter the difference in seconds")
+						fmt.Scanln(&secons)
+						secondsfunc(currenttime,secons)
+						break
+				default: 
+						fmt.Println("You didn't give any inputs!!")
+						break
+			}
 
-
-defer c.Close()
+		}
+	defer c.Close()
 }
 
+	func hoursfunc(currenttime time.Time,hors int64){
+		endtime := currenttime.Add(time.Hour*time.Duration(hors) +
+		time.Minute*time.Duration(00) +
+		time.Second*time.Duration(00))
+	    fmt.Println(string(endtime.Format("15:04")))
+	}
 
+	func minutesfunc(currenttime time.Time,minus int64){
+		endtime := currenttime.Add(time.Hour*time.Duration(00) +
+		time.Minute*time.Duration(minus) +
+		time.Second*time.Duration(00))
+	    fmt.Println(string(endtime.Format("15:04")))
+
+	}
+
+	func secondsfunc(currenttime time.Time,secons int64){
+		endtime := currenttime.Add(time.Hour*time.Duration(00) +
+		time.Minute*time.Duration(00) +
+		time.Second*time.Duration(secons))
+	    fmt.Println(string(endtime.Format("15:04")))
+	}
